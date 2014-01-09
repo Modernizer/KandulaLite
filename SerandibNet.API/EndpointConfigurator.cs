0f3112bbbeb1c7ec90810cd264e0b8d972e8c0a1
arshadn <arshadn@99x.lk>
2014-01-09 10:35:45 +0530

    Fixed dependency issues

--- SerandibNet.API/EndpointConfigurator.cs
+++ SerandibNet.API/EndpointConfigurator.cs
@@ -21,7 +21,7 @@ namespace SarandibNet.API
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=301869.
-            config.EnableQuerySupport(new QueryableAttribute() { MaxExpansionDepth = 5 });
+            //config.EnableQuerySupport(new QueryableAttribute() { MaxExpansionDepth = 5 });
 
             // To disable tracing in your application, please comment out or remove the following line of code
             // For more information, refer to: http://www.asp.net/web-api
